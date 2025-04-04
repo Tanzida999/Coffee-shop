61fa12c33de9fde151873f4b49bb2122013f8607
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-04-04 23:13:41 +0600

    Updated

--- coffee-shop/src/components/Hero/Hero.jsx
+++ coffee-shop/src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import BgImage from "../../assets/bg-slate.png";
+import BlackCoffee from "../../assets/black.png";
 const bgImage = {
   backgroundImage: `url(${BgImage})`,
   backgroundSize: "cover",
@@ -16,7 +17,32 @@ const Hero = () => {
         <div className="container">
           {/* navbar Section */}
           {/* Hero Section */}
-          <div className="grid grid-cols-1 md:grid-cols-2"></div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px]">
+            {/* text content section */}
+            <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
+              <h1 className="text-7xl font-bold leading-tight ml-14">
+                Black Tumbler
+              </h1>
+              <div className="relative">
+                <div className="relative z-10 space-y-4">
+                  <h1 className="text-2xl">Black Lifestyle Lovers</h1>
+                  <h1 className="text-sm opacity-55 loading-loose">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Alias commodi, nobis, porro, eligendi explicabo nostrum
+                    omnis dicta soluta quos illum aliquam aspernatur nemo.
+                    Cupiditate neque sint debitis repellendus perspiciatis
+                    doloribus!
+                  </h1>
+                </div>
+                <div className="absolute -top-6 -left-10 w-[25-px] h-[180px] bg-gray-700/25"></div>
+              </div>
+            </div>
+            {/* Hero Image  section */}
+            <div className="relative">
+              <img src={BlackCoffee} alt="" />
+            </div>
+            {/* Third DIvnsection */}
+          </div>
         </div>
       </section>
     </main>
