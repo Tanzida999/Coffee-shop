19fd12db5283c5bca07379565c04aed3a1571a2c
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-04-04 23:32:30 +0600

    Third Section

--- coffee-shop/src/components/Hero/Hero.jsx
+++ coffee-shop/src/components/Hero/Hero.jsx
@@ -53,6 +53,25 @@ const Hero = () => {
               </div>
             </div>
             {/* Third DIvnsection */}
+            <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
+              <h1 className="text-7xl font-bold leading-tight ml-14">
+                Black Tumbler
+              </h1>
+              <div className="relative">
+                <div className="relative z-10 space-y-4">
+                  <h1 className="text-2xl">Black Lifestyle Lovers</h1>
+                  <h1 className="text-sm opacity-55 loading-loose">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Alias commodi, nobis, porro, eligendi explicabo nostrum
+                    omnis dicta soluta quos illum aliquam aspernatur nemo.
+                    Cupiditate neque sint debitis repellendus perspiciatis
+                    doloribus!
+                  </h1>
+                </div>
+                <div className="absolute -top-6 -left-10 w-[25-px] h-[180px] bg-gray-700/25"></div>
+              </div>
+            </div>
+            <div></div>
           </div>
         </div>
       </section>
