eb22ed4664d04f941bbbc94d49c9ab290ed4cf3d
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-04-06 23:16:17 +0600

    Bug solved

--- coffee-shop/src/components/Hero/Hero.jsx
+++ coffee-shop/src/components/Hero/Hero.jsx
@@ -62,24 +62,52 @@ const Hero = () => {
             </div>
             {/* Hero Image  section */}
             <div className="relative">
-              <img
+              <motion.img
+                initial={{ opacity: 1, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 10,
+                  deplay: 0.4,
+                }}
                 src={BlackCoffee}
                 alt=""
                 className="relative z-40 h-[400px] md:h-[700px] img-shadow"
               />
               {/* orange ring circle */}
-              <div className="h-[180px] w-[180px] absolute top-24 -right-16 border-primary border-[20px] rounded-full z-10"></div>
-              <div className="absolute -top-20 left-[200px] z-[1]">
+              <motion.div
+                initial={{ opacity: 1, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 10,
+                  deplay: 0.8,
+                }}
+                className="h-[180px] w-[180px] absolute top-24 -right-16 border-primary border-[20px] rounded-full z-10"
+              ></motion.div>
+              <motion.div
+                initial={{ opacity: 1, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 10,
+                  deplay: 0.8,
+                }}
+                className="absolute -top-20 left-[200px] z-[-1]"
+              >
                 <motion.h1
                   initial={{ opacity: 0, y: -100 }}
-                  animate={{ opacity: 1, y: 0 }}
+                  animate={{ opacity: 1, x: 0 }}
                   className="text-[140px] scale-150 font-bold text-darkGray/40 leading-none"
                 >
                   Black Tumbler
                 </motion.h1>
-              </div>
+              </motion.div>
             </div>
-            {/* Third DIvnsection */}
+            {/* Third DIv section */}
             <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
               <h1 className="opacity-0 text-7xl font-bold leading-tight ml-14">
                 Black Tumbler
--- coffee-shop/src/components/Hero/Navbar/Navbar.jsx
+++ coffee-shop/src/components/Hero/Navbar/Navbar.jsx
@@ -1,19 +1,40 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <nav className="absolute top-0 left-0 w-full pt-10 text-white z-20">
+    <nav className="absolute top-0 left-0 w-full pt-10 text-white z-40">
       <div className="container">
-        <div className="">
+        <div className="flex justify-between items-center">
           {/* Logo section */}
-          <h1 className="text-2xl font-semibold uppercase">
+          <motion.h1
+            initial={{ opacity: 1, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              type: "spring",
+              stiffness: 100,
+              damping: 10,
+              delay: 0.2,
+            }}
+            className="text-2xl font-semibold uppercase"
+          >
             <span className="text-primary">Coders</span> Coffee
-          </h1>
+          </motion.h1>
           {/* Hamburger menu section */}
-          <div className="">
+          <motion.div
+            initial={{ opacity: 1, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              type: "spring",
+              stiffness: 100,
+              damping: 10,
+              delay: 0.2,
+            }}
+            className=""
+          >
             <GiHamburgerMenu className="text-3xl cursor-pointer"></GiHamburgerMenu>
-          </div>
+          </motion.div>
         </div>
       </div>
     </nav>
